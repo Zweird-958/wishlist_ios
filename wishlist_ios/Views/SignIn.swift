3d1e3b2d316ee637c8ff926bd946688bbbb8c80c
HAMEL Julien <julienhamel.h@gmail.com>
2023-08-02 15:39:47 +0200

    feat: navigation to signUp

--- wishlist_ios/Views/SignIn.swift
+++ wishlist_ios/Views/SignIn.swift
@@ -17,61 +17,75 @@ struct SignIn: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
+            ZStack {
                 VStack {
-                    Text("Sign In")
-                        .bold()
-                        .font(.system(.title, design: .monospaced))
-                        .foregroundColor(.blue)
-                    FormField(text: $email, placeholder: "Email")
-                    PasswordField(text: $password, placeholder: "Password")
-                }
-                .padding()
-
-                Button(action: {
-                    if isLoading {
-                        return
-                    }
-
-                    isLoading = true
-                    let bodyData = ["email": email, "password": password]
-                    let jsonData = try! JSONSerialization.data(withJSONObject: bodyData)
-
-                    apiCall(method: .post, path: "sign-in", body: jsonData) { (result: ApiResponse<String>) in
-
-                        switch result {
-                        case let .success(apiResponse):
-                            UserDefaults.standard.set(apiResponse, forKey: Config().keys.token)
-                            valid = true
-
-                        case let .failure(apiError):
-                            error = apiError
-                            showError.toggle()
-                            print(apiError)
-                        }
-                        isLoading = false
-                    }
-                }) {
-                    HStack(spacing: 8) {
-                        if isLoading {
-                            CircleLoader()
-                                .frame(width: 20, height: 20).foregroundColor(.white)
-                        }
+                    VStack {
                         Text("Sign In")
+                            .bold()
+                            .font(.system(.title, design: .monospaced))
+                            .foregroundColor(.blue)
+                        FormField(text: $email, placeholder: "Email")
+                        PasswordField(text: $password, placeholder: "Password")
                     }
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 40)
-                    .padding(.vertical, 10)
-                    .background(Color.blue)
-                    .cornerRadius(8)
+                    .padding()
+
+                    Button(action: {
+                        if isLoading {
+                            return
+                        }
+
+                        isLoading = true
+                        let bodyData = ["email": email, "password": password]
+                        let jsonData = try! JSONSerialization.data(withJSONObject: bodyData)
+
+                        apiCall(method: .post, path: "sign-in", body: jsonData) { (result: ApiResponse<String>) in
+
+                            switch result {
+                            case let .success(apiResponse):
+                                UserDefaults.standard.set(apiResponse, forKey: Config().keys.token)
+                                valid = true
+
+                            case let .failure(apiError):
+                                error = apiError
+                                showError.toggle()
+                                print(apiError)
+                            }
+                            isLoading = false
+                        }
+                    }) {
+                        HStack(spacing: 8) {
+                            if isLoading {
+                                CircleLoader()
+                                    .frame(width: 20, height: 20).foregroundColor(.white)
+                            }
+                            Text("Sign In")
+                        }
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 40)
+                        .padding(.vertical, 10)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                    }
                 }
-            }
-            .navigationDestination(isPresented: $valid) {
-                Wishlist()
-            }
-            .navigationBarHidden(true)
-            .alert(isPresented: $showError) {
-                Alert(title: Text("Error"), message: Text(error), dismissButton: .default(Text("Ok")))
+                .navigationDestination(isPresented: $valid) {
+                    Wishlist()
+                }
+                .navigationBarHidden(true)
+                .alert(isPresented: $showError) {
+                    Alert(title: Text("Error"), message: Text(error), dismissButton: .default(Text("Ok")))
+                }
+
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Text("Are you new ?")
+                        NavigationLink(destination: SignUp()) {
+                            Text("Register")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
             }
         }
     }
---
+++ wishlist_ios/Views/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  wishlist_ios
+//
+//  Created by Julien on 02/08/2023.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
