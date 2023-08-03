03a4c6d7bff22ef46ec1576617823cadb9420951
HAMEL Julien <julienhamel.h@gmail.com>
2023-08-03 12:19:32 +0200

    fix: recreate config file

---
+++ wishlist_ios/utils/config.swift
@@ -0,0 +1,16 @@
+//
+//  config.swift
+//  wishlist_ios
+//
+//  Created by Julien on 02/08/2023.
+//
+
+import Foundation
+
+struct Keys {
+    let token = "token"
+}
+
+struct Config {
+    let keys = Keys()
+}
